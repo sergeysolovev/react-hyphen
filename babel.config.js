df98ae60e20c321cb1da19d117e466dffdacf61f
Sergey Solovev <me@sergeysolovev.com>
2019-05-16 18:10:02 +0800

    Update babel.config.js to use default targets for env preset (#14)

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  presets: [
-    '@babel/preset-react',
-    [
-      '@babel/preset-env',
-      {
-        targets: {
-          node: 'current'
-        }
-      }
-    ]
-  ]
+  presets: ['@babel/preset-react', '@babel/preset-env']
 };
